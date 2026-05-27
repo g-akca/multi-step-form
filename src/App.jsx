8ef99901f29a7da3d6a673b82696f9b5487f4e65
g-akca <gok.akca66@gmail.com>
2026-05-27 19:03:29 +0300

    Create a currentStep state and pass functions to FormFooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import StepList from "./components/StepList";
 import FormSection from "./components/FormSection";
 import FormFooter from "./components/FormFooter";
+import { useState } from "react";
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+
   return (
     <div className="text-base leading-base text-grey-500 min-h-screen bg-blue-100 flex flex-col pt-8 bg-[url('/images/bg-sidebar-mobile.svg')] bg-no-repeat bg-top bg-contain">
       <main className="grow flex flex-col gap-8">
@@ -10,7 +13,10 @@ function App() {
 
         <form className="grow flex flex-col gap-6 justify-between px-4">
           <FormSection />
-          <FormFooter />
+          <FormFooter 
+            goNextStep={() => setCurrentStep(prev => prev + 1)} 
+            goPrevStep={() => setCurrentStep(prev => prev - 1)} 
+          />
         </form>
       </main>
     </div>
