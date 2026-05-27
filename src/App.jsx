5c796cf4afcb4480b47ea110f25fbd3ea2f90a89
g-akca <gok.akca66@gmail.com>
2026-05-27 19:21:17 +0300

    Add "Go Back" button to FormFooter, show Confirm button on 4th step

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         <form className="grow flex flex-col gap-6 justify-between px-4">
           <FormSection />
           <FormFooter 
+            currentStep={currentStep}
             goNextStep={goNextStep} 
             goPrevStep={goPrevStep} 
           />
--- src/components/FormFooter.jsx
+++ src/components/FormFooter.jsx
@@ -1,12 +1,22 @@
-function FormFooter({ goNextStep, goPrevStep }) {
+function FormFooter({ currentStep, goNextStep, goPrevStep }) {
   return (
-    <div className="fixed bottom-0 left-0 w-screen p-4 bg-white flex justify-end shadow-[0_25px_40px_rgba(0,0,0,0.095)]">
+    <div className={`fixed bottom-0 left-0 w-screen p-4 bg-white shadow-[0_25px_40px_rgba(0,0,0,0.095)] flex items-center ${currentStep >= 2 ? "justify-between" : "justify-end"}`}>
+      {currentStep >= 2 && (
+        <button
+          type="button"
+          onClick={goPrevStep}
+          className="leading-[120%] font-medium"
+        >
+          Go Back
+        </button>
+      )}
+
       <button 
         type="button" 
         onClick={goNextStep}
-        className="h-10 px-4 bg-blue-950 rounded-sm text-white text-[14px] leading-base font-medium"
+        className={`h-10 w-24.25 px-4 rounded-sm text-white text-[14px] leading-base font-medium ${currentStep <= 3 ? "bg-blue-950" : "bg-purple-600"}`}
       >
-        Next Step
+        {currentStep <= 3 ? "Next Step" : "Confirm"}
       </button>
     </div>
   )
