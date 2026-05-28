40136070f69930ad5edcac84b05cdb3f635a28de
g-akca <gok.akca66@gmail.com>
2026-05-28 16:04:23 +0300

    Add SwitchButton content and styles

--- src/components/steps/SwitchButton.jsx
+++ src/components/steps/SwitchButton.jsx
@@ -1,7 +1,11 @@
 function SwitchButton({ monthly, setMonthly }) {
   return (
-    <>
-    </>
+    <button 
+      type="button" 
+      onClick={() => setMonthly(prev => !prev)} className="bg-blue-950 p-1 h-5 w-9.5 rounded-[10px]"
+    >
+      <div className={`bg-white h-3 aspect-square rounded-full transition-all duration-200 ${monthly ? "translate-x-0" : "translate-x-4.5"}`} />
+    </button>
   )
 }
 
