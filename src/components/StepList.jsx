22165b29989add13a14151829d95ec15e4d914db
g-akca <gok.akca66@gmail.com>
2026-05-28 10:58:45 +0300

    Remove 5 from steps in StepList

--- src/components/StepList.jsx
+++ src/components/StepList.jsx
@@ -1,19 +1,17 @@
 import StepItem from "./StepItem";
 
-const steps = [1, 2, 3, 4, 5];
+const steps = [1, 2, 3, 4];
 
 function StepList({ currentStep }) {
   return (
     <div className="self-center flex gap-4 items-center">
-      {steps.map(step => {
-        if (step !== 5) return (
-          <StepItem
-            key={step}
-            number={step}
-            current={step === currentStep || (step === 4 && currentStep === 5)}
-          />
-        )
-      })}
+      {steps.map(step => (
+        <StepItem
+          key={step}
+          number={step}
+          current={step === currentStep || (step === 4 && currentStep === 5)}
+        />
+      ))}
     </div>
   )
 }
