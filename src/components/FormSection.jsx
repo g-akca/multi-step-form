303ad6c55d8afbbf941e07d2cf71e4f4431f5076
g-akca <gok.akca66@gmail.com>
2026-05-28 10:57:50 +0300

    Create Step2 component, add base structure

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,7 +1,9 @@
 import Step1 from "./Step1";
+import Step2 from "./Step2";
 
 const steps = {
-  1: <Step1 />
+  1: <Step1 />,
+  2: <Step2 />
 }
 
 function FormSection({ currentStep }) {
---
+++ src/components/Step2.jsx
@@ -0,0 +1,22 @@
+function Step2() {
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <h2 className="text-blue-950 text-[24px] leading-[120%] font-bold">Select your plan</h2>
+        <p>You have the option of monthly or yearly billing.</p>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+
+        </div>
+
+        <div className="bg-blue-50 rounded-lg">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Step2;
