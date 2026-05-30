4cddbd9167fbdb6d9871ca9de6f8531895a5495d
g-akca <gok.akca66@gmail.com>
2026-05-30 13:38:16 +0300

    Add Step5 content and styles

--- src/components/steps/Step5.jsx
+++ src/components/steps/Step5.jsx
@@ -1,6 +1,15 @@
+import thankYouIcon from "/images/icon-thank-you.svg";
+
 function Step5() {
   return (
     <>
+      <img src={thankYouIcon} alt="Thank you icon" className="w-14 self-center mt-12" />
+
+      <div className="flex flex-col gap-2 text-center mb-12">
+        <p className="text-[24px] leading-[120%] text-blue-950 font-bold">Thank you!</p>
+
+        <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
+      </div>
     </>
   )
 }
