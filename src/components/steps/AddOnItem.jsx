34b47cc77d40f04f95089faa76fcf8aa278956a7
g-akca <gok.akca66@gmail.com>
2026-05-30 22:25:52 +0300

    Change checkbox styles of AddOnItem

--- src/components/steps/AddOnItem.jsx
+++ src/components/steps/AddOnItem.jsx
@@ -2,7 +2,13 @@ function AddOnItem({ addon, monthly, register }) {
   return (
     <label className="px-4 py-[13.5px] flex gap-4 items-center justify-between rounded-lg bg-white border border-purple-200 has-checked:bg-blue-50 has-checked:border-purple-600">
       <div className="flex gap-4 items-center">
-        <input type="checkbox" name="addon" id={addon.id} {...register(addon.id)} />
+        <input 
+          type="checkbox" 
+          name="addon" 
+          id={addon.id} 
+          className="h-5 aspect-square rounded-sm accent-purple-600" 
+          {...register(addon.id)} 
+        />
 
         <div>
           <p className="text-blue-950 text-[14px] leading-base font-medium">{addon.name}</p>
