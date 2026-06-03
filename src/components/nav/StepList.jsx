e32d5ef1c70b54562200cb71a329ec0230748b77
g-akca <gok.akca66@gmail.com>
2026-06-03 22:05:25 +0300

    Add tablet responsive StepList styles

--- src/components/nav/StepList.jsx
+++ src/components/nav/StepList.jsx
@@ -4,14 +4,16 @@ const steps = [1, 2, 3, 4];
 
 function StepList({ currentStep }) {
   return (
-    <div className="self-center flex gap-4 items-center">
-      {steps.map(step => (
-        <StepItem
-          key={step}
-          number={step}
-          current={step === currentStep || (step === 4 && currentStep === 5)}
-        />
-      ))}
+    <div className="self-center flex gap-4 items-center tablet:self-stretch tablet:items-stretch tablet:p-4">
+      <div className="contents tablet:flex tablet:flex-col tablet:gap-8 tablet:py-10 tablet:px-6 tablet:bg-[url('/images/bg-sidebar-desktop.svg')] tablet:bg-no-repeat tablet:bg-cover tablet:bg-bottom-left tablet:rounded-[10px]">
+        {steps.map(step => (
+          <StepItem
+            key={step}
+            number={step}
+            current={step === currentStep || (step === 4 && currentStep === 5)}
+          />
+        ))}
+      </div>
     </div>
   )
 }
