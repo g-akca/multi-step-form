65e316bb683f77d73c8e27b37aa8082f7a3bbcb8
g-akca <gok.akca66@gmail.com>
2026-06-04 12:11:13 +0300

    Make StepList not shrinkable

--- src/components/nav/StepList.jsx
+++ src/components/nav/StepList.jsx
@@ -21,7 +21,7 @@ const steps = [
 
 function StepList({ currentStep }) {
   return (
-    <div className="self-center flex gap-4 items-center tablet:self-stretch tablet:items-stretch tablet:p-4">
+    <div className="shrink-0 self-center flex gap-4 items-center tablet:self-stretch tablet:items-stretch tablet:p-4">
       <div className="contents tablet:flex tablet:flex-col tablet:gap-8 tablet:py-10 tablet:px-6 tablet:bg-[url('/images/bg-sidebar-desktop.svg')] tablet:bg-no-repeat tablet:bg-cover tablet:bg-bottom-left tablet:rounded-[10px]">
         {steps.map(step => (
           <StepItem
