c5ef4422debb02c814e5064c5af0a32b79a3513e
g-akca <gok.akca66@gmail.com>
2026-06-04 12:09:04 +0300

    Add tablet responsive StepItem content and styles

--- src/components/nav/StepItem.jsx
+++ src/components/nav/StepItem.jsx
@@ -1,7 +1,14 @@
 function StepItem({ number, title, isCurrent }) {
   return (
-    <div className={`rounded-full w-8.25 aspect-square flex justify-center items-center text-[14px] leading-[120%] tracking-[-1px] font-bold ${isCurrent ? "bg-blue-200 text-blue-950" : "text-white border border-white"}`}>
-      {number}
+    <div className="contents tablet:flex tablet:gap-4 tablet:items-center">
+      <div className={`rounded-full w-8.25 aspect-square flex justify-center items-center text-[14px] leading-[120%] tracking-[-1px] font-bold ${isCurrent ? "bg-blue-200 text-blue-950" : "text-white border border-white"}`}>
+        {number}
+      </div>
+
+      <div className="hidden tablet:flex tablet:flex-col tablet:uppercase">
+        <p className="text-blue-300 text-[12px] leading-[130%]">Step {number}</p>
+        <p className="text-white text-[14px] leading-[120%] font-bold tracking-[1px]">{title}</p>
+      </div>
     </div>
   )
 }
