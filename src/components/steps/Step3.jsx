9a6259d3b871430bacdff3361df7f13ac9c18f1c
g-akca <gok.akca66@gmail.com>
2026-06-08 14:54:05 +0300

    Add tablet responsive Step3 styles

--- src/components/steps/Step3.jsx
+++ src/components/steps/Step3.jsx
@@ -8,11 +8,11 @@ function Step3({ addons }) {
   return (
     <>
       <div className="flex flex-col gap-2">
-        <h2 className="text-blue-950 text-[24px] leading-[120%] font-bold">Pick add-ons</h2>
+        <h2 className="text-blue-950 text-[24px] leading-[120%] font-bold tablet:text-[32px]">Pick add-ons</h2>
         <p>Add-ons help enhance your gaming experience.</p>
       </div>
 
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 tablet:gap-4">
         {addons.map(addon => (
           <AddOnItem
             key={addon.id}
