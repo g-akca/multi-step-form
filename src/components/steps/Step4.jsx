017910409f0bfc4ad55f02eada6b02ce345d8a6d
g-akca <gok.akca66@gmail.com>
2026-06-15 12:15:47 +0300

    Add active styles to Step4, improve code readability

--- src/components/steps/Step4.jsx
+++ src/components/steps/Step4.jsx
@@ -32,11 +32,22 @@ function Step4({ plans, addons, goToStep2 }) {
         <div className="bg-blue-50 p-4 rounded-lg flex flex-col gap-4 desktop:px-6">
           <div className="flex gap-4 justify-between items-center tablet:px-1 desktop:px-0">
             <div className="flex flex-col items-start tablet:gap-2">
-              <p className="leading-base text-blue-950 font-medium tablet:text-base tablet:leading-[120%]">{selectedPlan.name} ({billing === "monthly" ? "Monthly" : "Yearly"})</p>
-              <button type="button" onClick={goToStep2}>Change</button>
+              <p className="leading-base text-blue-950 font-medium tablet:text-base tablet:leading-[120%]">
+                {selectedPlan.name} ({billing === "monthly" ? "Monthly" : "Yearly"})
+              </p>
+              
+              <button 
+                type="button" 
+                onClick={goToStep2} 
+                className="cursor-pointer transition-all duration-300 hover:text-purple-600"
+              >
+                Change
+              </button>
             </div>
 
-            <p className="text-blue-950 font-bold tablet:text-base tablet:leading-5">${planPrice}/{billing === "monthly" ? "mo" : "yr"}</p>
+            <p className="text-blue-950 font-bold tablet:text-base tablet:leading-5">
+              ${planPrice}/{billing === "monthly" ? "mo" : "yr"}
+            </p>
           </div>
 
           {selectedAddons.length > 0 && (
@@ -47,7 +58,9 @@ function Step4({ plans, addons, goToStep2 }) {
                 <div className="flex gap-4 items-center justify-between tablet:px-1 desktop:px-0">
                   <p>{addon.name}</p>
                   
-                  <p className="text-blue-950">+${billing === "monthly" ? `${addon.price.monthly}/mo` : `${addon.price.yearly}/yr`}</p>
+                  <p className="text-blue-950">
+                    +${billing === "monthly" ? `${addon.price.monthly}/mo` : `${addon.price.yearly}/yr`}
+                  </p>
                 </div>
               ))}
             </>
